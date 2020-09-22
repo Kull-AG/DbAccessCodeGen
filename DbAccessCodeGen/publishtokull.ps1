77c5af901771d8884d0c824f26053f9107efabfd
Adrian Ehrsam <aersamkull@users.noreply.github.com>
2020-09-22 11:31:15 +0200

    Delete publishtokull.ps1
    No longer needed

--- DbAccessCodeGen/publishtokull.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item -Recurse -Force nupkg
-dotnet pack
-$file = (Get-ChildItem nupkg)[0]
-$file
-dotnet nuget push --source "https://pkgs.dev.azure.com/kull-ag-aarau/_packaging/kull-ag-aarau/nuget/v3/index.json" nupkg/($file.Name) --api-key az
