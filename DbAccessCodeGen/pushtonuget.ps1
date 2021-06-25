97497a1bea647cec2eebc18c4e0019f25193dee4
Adrian Ehrsam <ehrsam@kull.ch>
2021-06-25 06:21:53 +0200

    push new version

--- DbAccessCodeGen/pushtonuget.ps1
+++ DbAccessCodeGen/pushtonuget.ps1
@@ -1,2 +1,6 @@
+Get-ChildItem "$PSScriptRoot/nupkg" | Remove-Item
 dotnet pack --configuration Release
-dotnet nuget push .\nupkg\*.nupkg --source https://api.nuget.org/v3/index.json --api-key $env:Nuget_API_KEY_Personal
+
+Get-ChildItem "$PSScriptRoot/nupkg" | ForEach-Object {
+	dotnet nuget push $_.FullName --source https://api.nuget.org/v3/index.json --api-key $env:Nuget_API_KEY_Personal
+}
